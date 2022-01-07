2dc560a51d6a0bafa57ec0870f9018d0f4c61ac2
幾個秋Te-Wei Chiang（小威老師談電腦） <tewei0328@gmail.com>
2022-01-07 13:38:15 +0800

    Update test2-1.py

--- test2-1.py
+++ test2-1.py
@@ -4,5 +4,5 @@ import function
 # 取得當天日期
 Date=datetime.datetime.now().strftime("%Y%m%d")
 
-for i in function.getSIDMatch('20220106','TX00'):
+for i in function.getSIDMatch(Date,'TX00'):
   print(i)
